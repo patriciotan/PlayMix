55b133f24064e84b668a3a1f7f37d622e416773e
patriciotan <xanpatan@gmail.com>
2014-11-21 18:43:56 +0800

    joins done

--- application/views/feed_view.php
+++ application/views/feed_view.php
@@ -40,9 +40,14 @@
     <?php foreach($rec->result() as $row): ?>
 
 
-    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/control_play.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
-    &nbsp;<?php echo $row->audio_title; ?> 
+    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/play.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
+    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/plus.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
+    &nbsp;<?php echo $row->audio_title; ?> by 
+    &nbsp;<?php echo $row->user_username; ?>, 
+    &nbsp;<?php echo $row->audio_date_added; ?> 
     &nbsp;<?php echo $row->audio_play_count; ?> plays</p></li>
+    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/share.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
+    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/download.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
 
 
 
