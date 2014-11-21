c33e0429e908531becc1341001abbb3b1fc538ec
eufrik <eufr1k@gmail.com>
2014-11-21 23:19:00 +0800

    finished feed

--- application/views/feed_view.php
+++ application/views/feed_view.php
@@ -4,19 +4,22 @@
 <div class="container" > <!-- This is the div that contains the most-played songs according to the database -->
 
 <div class="centered" style="position:relative; top:150px; width:870px;"><ul style="width:500px;"><h1>FEED</h1></ul>
-    <?php foreach($rec->result() as $row): ?>
 
-    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/play.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
-    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/plus.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
-    &nbsp;<?php echo $row->audio_title; ?> by 
-    &nbsp;<?php echo $row->user_username; ?>, 
-    &nbsp;<?php echo $row->audio_date_added; ?> 
-    &nbsp;<?php echo $row->audio_play_count; ?> plays</p></li>
-    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/share.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
-    <a href="#"><li><p><img src="<?php echo base_url(); ?>assets/download.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> 
-    
-    <?php endforeach;
-    ?>
+  <table class="feed_table">  
+    <?php foreach($rec->result() as $row): ?>
+    <tr class="<?php echo alternator('background:#cfc','background:#ffc'); ?>">
+      <td><a href="#"><img src="<?php echo base_url(); ?>assets/play.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a></td>
+      <td><a href="#"><img src="<?php echo base_url(); ?>assets/plus.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a></td>
+      <td><a href="#"><?=$row->audio_title;?></a></td>
+      <td style="width:10px; padding-right:20px">by</td>
+      <td ><a href="#"><?=$row->user_username;?></a></td>
+      <td ><?=$row->audio_date_added;?></td>
+      <td ><?=$row->audio_play_count;?></td>
+      <td><a href="#"><img src="<?php echo base_url(); ?>assets/share.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a> </td>
+      <td><a href="#"><img src="<?php echo base_url(); ?>assets/download.ico" style="float:left;margin-top:5px;z-index:5" alt="logo"/></a></td>
+    </tr>
+    <?php endforeach;?>
+
 </div>
 </div>
 </div>
