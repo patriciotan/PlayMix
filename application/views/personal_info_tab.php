64f8e3da57d16ee1cb849b93be1528d7fd212ed5
eufrik <eufr1k@gmail.com>
2014-12-18 23:35:13 +0800

    temporary prof image

--- application/views/personal_info_tab.php
+++ application/views/personal_info_tab.php
@@ -3,12 +3,27 @@
 <div class="centered">
 	<div id="image">
 		<div id="img_container" class="profile-circular-mask">
-			<img class="img_profile" src="img/profile/1.png"/>
+			<img class="img_profile" src="<?php echo base_url(); ?>assets/img/profile/1.png"/>
 		</div>
 	</div>
+
+
 	<div id="info">
+		<div id="user_name">
+			<h1><?php ?></h1>&nbsp;<h1><?php ?></h1>
+		</div>
+		<div id="user_country">
+		</div>
+		<div id="user_bio">
+		</div>
 	</div>
+
+
 	<div id="websites">
+		<div id="user_sites">
+			<ul>
+			</ul>
+		</div>
 	</div>
 
 
